de7f1702b9de8bf77f031b7a7772caaadb13c595
Mustafa Ahmed <mustafaahmedsecuremail@gmail.com>
2025-08-24 14:53:06 +0500

    QuickReport Incident Reporting Website : Updates some designing in Apps pages, components etc

--- components/Report/ReportFormCompleted.tsx
+++ components/Report/ReportFormCompleted.tsx
@@ -47,10 +47,10 @@ export function ReportSubmitted({ data }: ReportSubmittedProps) {
 
       <div className="pt-4">
         <button
-          onClick={() => (window.location.href = "/")}
+          onClick={() => (window.location.href = "/track-report")}
           className="inline-flex items-center justify-center rounded-lg bg-sky-500 px-4 py-2 text-sm font-medium text-white hover:bg-sky-400 cursor-pointer"
         >
-          Return to Home
+          Track Your Report
         </button>
       </div>
     </div>
